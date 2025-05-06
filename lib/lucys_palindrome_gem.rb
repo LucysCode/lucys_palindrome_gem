32a6ecec2a0f3215df6db0f758ea58a2f09d685b
Lucy C <lucychen96@gmail.com>
2025-05-05 22:45:55 -0400

    Initialize repository

---
+++ lib/lucys_palindrome_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lucys_palindrome_gem/version"
+
+module LucysPalindromeGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lucys_palindrome_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LucysPalindromeGem
+  VERSION = "0.1.0"
+end
