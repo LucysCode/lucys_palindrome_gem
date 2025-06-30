71ba4d33820f554359be5087b77dca29eb7fac8a
Lucy C <lucychen96@gmail.com>
2025-06-29 21:14:35 -0400

    Add custom tests into lucys_palindrome_gem.rb

--- lib/lucys_palindrome_gem.rb
+++ lib/lucys_palindrome_gem.rb
@@ -2,7 +2,23 @@
 
 require_relative "lucys_palindrome_gem/version"
 
-module LucysPalindromeGem
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    #Returns content for palindrome testing.
+    def processed_content
+      self.downcase
+    end
 end
+
+
+# # Default from gem creation
+# module LucysPalindromeGem
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
--- test/test_lucys_palindrome_gem.rb
+++ test/test_lucys_palindrome_gem.rb
@@ -3,11 +3,29 @@
 require "test_helper"
 
 class TestLucysPalindromeGem < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::LucysPalindromeGem::VERSION
+  # # Default test code from gem creation
+  # def test_that_it_has_a_version_number
+  #   refute_nil ::LucysPalindromeGem::VERSION
+  # end
+
+  # def test_it_does_something_useful
+  #   assert false
+  # end
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip # these are pending tests, which are placeholders/reminders for tests we want to write
+  end
+
 end
